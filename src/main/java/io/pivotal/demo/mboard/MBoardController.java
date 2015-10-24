9aa0ee4579b381e9ad634e9070d5f71e0c82a28c
Kris De Volder <kdevolder@pivotal.io>
2015-10-24 15:19:51 -0700

    Cleanup imports

--- src/main/java/io/pivotal/demo/mboard/MBoardController.java
+++ src/main/java/io/pivotal/demo/mboard/MBoardController.java
@@ -4,13 +4,11 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
