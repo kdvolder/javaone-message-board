568fe5760592e866c46a0049d6d38d0adb7aaaaa
Kris De Volder <kdevolder@pivotal.io>
2015-10-24 15:03:56 -0700

    Fix Message entity to have all the stuff it needs to have

--- src/main/java/io/pivotal/demo/mboard/data/Message.java
+++ src/main/java/io/pivotal/demo/mboard/data/Message.java
@@ -1,34 +1,58 @@
 package io.pivotal.demo.mboard.data;
 
+import java.util.Calendar;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
 public class Message {
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
-	private long id;
-	
+	private Long id;
+
+	@NotEmpty(message = "Message is required.")
+	private String text;
+
+	@NotEmpty(message = "Summary is required.")
 	private String summary;
-	private String description;
-	
-	public String getSummary() {
-		return summary;
+
+	private Calendar created = Calendar.getInstance();
+
+	public Long getId() {
+		return this.id;
 	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Calendar getCreated() {
+		return this.created;
+	}
+
+	public void setCreated(Calendar created) {
+		this.created = created;
+	}
+
+	public String getText() {
+		return this.text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public String getSummary() {
+		return this.summary;
+	}
+
 	public void setSummary(String summary) {
 		this.summary = summary;
 	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	public long getId() {
-		return id;
-	}
-	
 }
