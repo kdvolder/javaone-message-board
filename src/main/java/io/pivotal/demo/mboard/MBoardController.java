50f21af3fd316399257efcb277584c5dddd3bd54
Kris De Volder <kdevolder@pivotal.io>
2015-10-24 18:33:03 -0700

    Add a commented request mapping so we can add it easily

--- src/main/java/io/pivotal/demo/mboard/MBoardController.java
+++ src/main/java/io/pivotal/demo/mboard/MBoardController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -52,6 +53,12 @@ public class MBoardController {
 		redirect.addFlashAttribute("globalMessage", "Successfully created a new message");
 		return new ModelAndView("redirect:/{message.id}", "message.id", message.getId());
 	}
+	
+//	@RequestMapping("test")
+//	@ResponseBody
+//	public String test() {
+//		return "This is a TEST";
+//	}
 
 	@RequestMapping("foo")
 	public String foo() {
